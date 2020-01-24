f4ca9fbece5f3be837ee2607b69fbd70b62a59e4
Francois Huyghe <francois@13milliseconds.com>
2020-01-24 13:06:15 -0600

    Fixed ACF installation

--- web/app/themes/thememorypalace2020/app/Controllers/FrontPage.php
+++ web/app/themes/thememorypalace2020/app/Controllers/FrontPage.php
@@ -7,12 +7,22 @@ use WP_Query;
 
 class FrontPage extends Controller
 {
-
-    public function latest_episodes() {
+	public function latest_episode() {
 	    $args = array(
 	    	'post_type' => 'post',
 	    	//'orderby'	=> 'rand',
-	    	'posts_per_page' => 4,
+	    	'posts_per_page' => 1,
+	    );
+	    $the_query = new WP_Query( $args );
+	    return $the_query->posts;
+	}
+
+    public function episodes() {
+	    $args = array(
+	    	'post_type' => 'post',
+	    	//'orderby'	=> 'rand',
+			'posts_per_page' => 4,
+			'offset' => 1
 	    );
 	    $the_query = new WP_Query( $args );
 	    return $the_query->posts;
--- web/app/themes/thememorypalace2020/resources/views/partials/content-home.blade.php
+++ web/app/themes/thememorypalace2020/resources/views/partials/content-home.blade.php
@@ -1,11 +1,14 @@
 <section id="top">
-    Top player
+    <div class="latest-episode">
+        @php $episode = $latest_episode[0] @endphp
+        @include('partials.episode-block')
+    </div>
 </section>
 
 <section id="episodes">
     <div class="container">
         <h2>Episodes</h2>
-        @foreach ($latest_episodes as $episode)
+        @foreach ($episodes as $episode)
             @include('partials.episode-block')
         @endforeach
         <a class="button" href="/episodes">More Episodes</a>
