49b40dade09bd2e97594e0862a8d8063f699e7d4
Francois Huyghe <francois@13milliseconds.com>
2020-07-09 18:01:03 -0500

    New post layout

--- web/app/themes/thememorypalace2020/resources/views/partials/content-single.blade.php
+++ web/app/themes/thememorypalace2020/resources/views/partials/content-single.blade.php
@@ -1,15 +1,16 @@
 @include('partials.color-brightness')
 
-<section id="top" class="{{ the_field('color') }}">
-  <div class="container">
-    @php $episode = $post; @endphp
-    @include('partials.episode-block')
-  </div>
-</section>
 <section id="episodeContent">
   <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        @php $episode = $post; @endphp
+        @include('partials.episode-block')
+      </div>
+      <div class="col-md-8">
       <article {!! post_class() !!}>
         <div class="entry-content">
+          <h1>{{ the_title() }}</h1>
           <p>Published on {{ get_the_date('', $episode->ID) }} </p>
           @php the_content() @endphp
           <div class="notes">
@@ -17,5 +18,6 @@
           </div>
         </div>
       </article>
+    </div>
   </div>
 </section>
