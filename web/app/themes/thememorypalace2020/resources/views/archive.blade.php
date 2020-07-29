0c5f08210057ebc977c9f905082ce5396ae643d5
Francois Huyghe <francois@13milliseconds.com>
2020-07-29 17:30:51 -0500

    Category page

--- web/app/themes/thememorypalace2020/resources/views/archive.blade.php
+++ web/app/themes/thememorypalace2020/resources/views/archive.blade.php
@@ -1,9 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
-    <div class="container">
-      @include('partials.content-archive')
+
+  <div class="container">
+  <section id="intro">
+    <h1>{{ single_cat_title() }}</h1>
+    @php the_archive_description( '<div class="taxonomy-description">', '</div>' );  @endphp
+  </section>
+
+  <section id="archive">
+    <div class="episodes">
+        <div class="gutter-sizer"></div>
+        @while(have_posts()) @php the_post() @endphp
+            @php $episode = get_post() @endphp
+            @include('partials.episode-block')
+        @endwhile
     </div>
-  @endwhile
+  </section>
+  </div>
 @endsection
